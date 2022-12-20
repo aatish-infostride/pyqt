6a85892b13011fa2200d8871b771e0ddf4f0c3d6
aatish <aatish.vaid@infostride.com>
2022-12-21 03:33:28 +0530

    second commit

--- main.py
+++ main.py
@@ -3,11 +3,13 @@
 
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QVBoxLayout, QLabel,QLCDNumber
+from PyQt5.QtWidgets import QDialog, QApplication,QMainWindow, QVBoxLayout, QLabel,QLCDNumber, QMessageBox, QWidget
 from PyQt5.uic import loadUi
 from PyQt5.QtCore import QTimer
 from PyQt5.QtGui import QFont
 from PyQt5 import QtCore, QtGui
+import pyautogui
+import subprocess
 
 
 class Login(QDialog):
@@ -49,17 +51,40 @@ class CreateAcc(QDialog):
             widget.addWidget(login)
             widget.setCurrentIndex(widget.currentIndex()+1)
 
-class dashboard(QDialog):
+class dashboard(QMainWindow):
     def __init__(self):
         super(dashboard,self).__init__()
-        loadUi("dashboard.ui", self)
+        loadUi("interface.ui", self)
         # self.punchinbutton.clicked.connect(self.start_timer)
+
+
+        self.idle_time = 0
+        self.notification_shown = False
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.check_idle_time)
+        self.timer.start(1000)  # Check idle time every second
+
+
+    def check_idle_time(self):
+        if sys.platform == "win32":
+            self.idle_time = pyautogui.idle()
+        else:
+            output = subprocess.check_output(["xprintidle"])
+            print(output)
+            self.idle_time = int(output) / 1000.0
+            
+
+        if self.idle_time > 60 and not self.notification_shown:
+            print(f"The system has been idle for {self.idle_time} seconds.")
+            self.show_notification()
+            self.notification_shown = True
+
+    def show_notification(self):
+        message_box = QMessageBox()
+        message_box.setText("The system has been idle for more than 60 seconds.")
+        message_box.exec_()
         
-    
-
-    
-
-
     
 
 
